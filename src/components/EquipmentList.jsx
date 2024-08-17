d5d8cbf5c4277083c76d4a429e68bd0de118f235
Johnnson <af7er.g@gmail.com>
2024-08-17 23:36:45 +0200

    added

--- src/components/EquipmentList.jsx
+++ src/components/EquipmentList.jsx
@@ -1,12 +1,16 @@
 export default function Equipment() {
 
+    const listOfUsedEquipment = ["Loggers", "Machines", "Electronics", "Meters"];
+
     const eqSampleData1 = {
         name: "Machine 1",
-        
+
     }
 
     return (<>
-    <p>Here will b e the equipment mapped</p>
+        <p>Here will b e the equipment mapped:</p>
+        <ul>
+        </ul>
     </>)
 
 }
--- src/components/Sample.jsx
+++ src/components/Sample.jsx
@@ -1,7 +1,6 @@
 export default function SampleData() {
     const profileOne = [{ am: 1, fr: 5 }, { am: 2, fr: 10 }, { am: 2, fr: 20 }, { am: 1, fr: 50 }]
 
-
     return (<>
 
         <p>the profile one:</p>

d337fcb1794bdfdecb5c8afc37274f759911edda
Johnnson <af7er.g@gmail.com>
2024-08-20 22:24:04 +0200

    updates for components bizz

--- src/components/EquipmentList.jsx
+++ src/components/EquipmentList.jsx
@@ -1,15 +1,30 @@
 export default function Equipment() {
 
+    // where should I use state?
+
     const listOfUsedEquipment = ["Loggers", "Machines", "Electronics", "Meters"];
 
     const eqSampleData1 = {
         name: "Machine 1",
 
-    }
+    };
+
+    // array of data for rendering:
+    const people = [
+        'Creola Katherine Johnson: mathematician',
+        'Mario José Molina-Pasquel Henríquez: chemist',
+        'Mohammad Abdus Salam: physicist',
+        'Percy Lavon Julian: chemist',
+        'Subrahmanyan Chandrasekhar: astrophysicist'
+    ];
+
+    const listItems = people.map( v => <li>{v}</li>)
 
     return (<>
         <p>Here will b e the equipment mapped:</p>
         <ul>
+            {/* Here I am feeding the program with the items I've mentioned above */}
+            {listItems}
         </ul>
     </>)
 

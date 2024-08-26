c2cbdabe64cbeb1d8eed90fced55f1d4fbbe139f
Johnnson <af7er.g@gmail.com>
2024-08-26 20:26:14 +0200

    components added

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,12 +1,19 @@
 import './frontpage.css';
+import { useState } from 'react';
 
 export default function Front() {
+
+    const [index, setIndex] = useState(0);
+    const [showMore, setShowMore] = useState(false);
+
 
     return (
 
         <main>
             <h1>Mediterranian sea beauty meets forest!</h1>
 
+            <span>{index}</span>
+            <span>{showMore}</span>
             <p>Welcome camping in most heavenly place! Enjoy the peace and get rest.</p>
 
         </main>
